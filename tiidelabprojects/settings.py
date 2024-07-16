1cdc034e12363ea77750f7d7233cd72af183d231
TIJANI0826 <ibrahimtijani08@gmail.com>
2024-07-16 17:24:38 +0100

    change app password

--- tiidelabprojects/settings.py
+++ tiidelabprojects/settings.py
@@ -129,6 +129,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ibrahimtijani08@gmail.com'
-EMAIL_HOST_PASSWORD = 'qoollmurweuslqoh'
+EMAIL_HOST_PASSWORD = 'busozujstdszfurp'
 DEFAULT_FROM_EMAIL = 'ibrahimtijani08@gmail.com'
+EMAIL_USE_SSL = False
+
 
